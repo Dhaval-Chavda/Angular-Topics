f6c82eea1d576114ecef244b068b19fc88fd613a
Dhaval150 <d.r.chavda1509@gmail.com>
2023-05-20 17:49:51 +0530

    Angular-Topics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Topics';
+
+  data = "Dhaval Chavda";
+
+  // get()
+  // {
+  //   return "Get Function";
+  // }
+
+  // displayVal='';
+
+  // getValue(val:string)
+  // {
+  //   console.log(val);
+  //   this.displayVal=val;
+  // }
+
+
+  // name = 'dhaval';
+
+  // disable = false;
+
+  // show = "Dhaval";
+
+  // color = "red";
+
+  //Loops Aray & Objects
+
+  Users = ['Dhaval', 'Parth', 'Haresh', 'Ankit', 'Niraj', 'Akash', 'Dhruv', 'Shahil'];
+
+  UserDetails = [
+
+    {name: 'Dhaval', email: 'Dhaval@gmail.com', Phone: '9909638648' },
+    {name: 'Dhruv', email: 'Dhruv@gmail.com', Phone: '9909638669' },
+    {name: 'Krutik', email: 'Krutik@gmail.com', Phone: '9909638650' },
+    {name: 'Parth', email: 'Parth@gmail.com', Phone: '9909638645' },
+
+  ]
+
 }
