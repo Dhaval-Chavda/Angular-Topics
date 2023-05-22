7c5b826c542afc8ca59dbde159ae67ced7fc513d
Dhaval150 <d.r.chavda1509@gmail.com>
2023-05-22 18:27:02 +0530

    Angular-Topics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -32,17 +33,82 @@ export class AppComponent {
 
   // color = "red";
 
-  //Loops Aray & Objects
+  //////////////////////////////////////////////////////////////  Loops Aray & Objects  ///////////////////////////////////////////////////////////////////////////////////////
 
-  Users = ['Dhaval', 'Parth', 'Haresh', 'Ankit', 'Niraj', 'Akash', 'Dhruv', 'Shahil'];
 
-  UserDetails = [
+  // Users = ['Dhaval', 'Parth', 'Haresh', 'Ankit', 'Niraj', 'Akash', 'Dhruv', 'Shahil'];
 
-    {name: 'Dhaval', email: 'Dhaval@gmail.com', Phone: '9909638648' },
-    {name: 'Dhruv', email: 'Dhruv@gmail.com', Phone: '9909638669' },
-    {name: 'Krutik', email: 'Krutik@gmail.com', Phone: '9909638650' },
-    {name: 'Parth', email: 'Parth@gmail.com', Phone: '9909638645' },
+  // UserDetails = [
 
-  ]
+  //   {name: 'Dhaval', email: 'Dhaval@gmail.com', Phone: '9909638648' },
+  //   {name: 'Dhruv', email: 'Dhruv@gmail.com', Phone: '9909638669' },
+  //   {name: 'Krutik', email: 'Krutik@gmail.com', Phone: '9909638650' },
+  //   {name: 'Parth', email: 'Parth@gmail.com', Phone: '9909638645' },
+
+  // ]
+
+
+////////////////////////////////////////////////////////////////   Nested Loops  ////////////////////////////////////////////////////////////////////////////////////
+
+  // users = [
+  //     {name:'Dhaval', phone:'2112' , socialAccounts: ['Whats Up','faceBook','Instagram']},
+  //     {name:'Dhruv', phone:'3333' , socialAccounts: ['Youtube','faceBook','Instagram']},
+  //     {name:'Haresh', phone:'4444' , socialAccounts: ['Whats Up','Linkedin','Instagram']},
+  //     {name:'Akash', phone:'5555' , socialAccounts: ['Whats Up','faceBook','Instagram']},
+  // ]
+
+
+////////////////////////////////////////////////////////////////////  style binding  ///////////////////////////////////////////////////////////////////////////
+
+  // color="red";
+  // bgcolor = "green";
+
+  // updateColor()
+  // {
+  //   this.color='blue';
+  //   this.bgcolor="black";
+  
+  // }
+
+
+///////////////////////////////////////////////////////////////// Basic Form /////////////////////////////////////////////////////////////////////////////
+
+
+  // userData:any={};
+
+  // getData(data:any)
+  // {
+  //   console.log(data);
+  //   this.userData=data;
+  // }
+
+////////////////////////////////////////////////////////////////  Toggle Element  ///////////////////////////////////////////////////////////////////
+
+// display=true;
+
+// toggle(){
+//   this.display=!this.display;
+// }
+
+
+
+////////////////////////////////////////////////////////////////  Make Simple TO-DO  ///////////////////////////////////////////////////////////////////
+
+
+// list:any[] = [];
+
+// addTask(item:string)
+// {
+ 
+//   this.list.push({id:this.list.length, name:item});
+//   console.log(this.list);
+// }
+
+// Remove(id:number){
+//   console.log(id);
+//       this.list=this.list.filter(item => item.id!==id)
+// }
+
+nm:any;
 
 }
